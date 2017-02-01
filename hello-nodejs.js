cc67236b0346bac045c3e5c32b00e067980253dd
medhane <medhane>
2017-02-01 19:46:29 +0530

    Simple HelloNodeJS example, routing, parse url components

---
+++ hello-nodejs.js
@@ -0,0 +1,10 @@
+
+
+var express = require("express");
+var app = express();
+app.get("/", function(request, response) {
+ response.send("Hello, friends!");
+});
+app.listen(3000, function() {
+ console.log("Express app started on port 3000.");
+});
---
+++ url-test.js
@@ -0,0 +1,5 @@
+var url = require("url");
+var parsedURL = url.parse("http://www.example.com/profile?name=barry");
+console.log(parsedURL.protocol); // "http:"
+console.log(parsedURL.host); // "www.example.com"
+console.log(parsedURL.query); // "name=barry"

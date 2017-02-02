d7dbdec9292ffa6115c855967e8ee6c72bbea425
Rohini <rohinijadhavdeopur@gmail.com>
2017-02-02 19:48:28 +0530

    simple expressjs file with morgan module, html code.

---
+++ http-methods.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+app.get("/", function(req, res) {
+ res.send("you just sent a GET request, friend");
+});
+app.post("/", function(req, res) {
+ res.send("a POST request? nice");
+});
+app.put("/", function(req, res) {
+ res.send("i don’t see a lot of PUT requests anymore");
+});
+app.delete("/", function(req, res) {
+ res.send("oh my, a DELETE??");
+});
+app.listen(3000, function() {
+ console.log("App is listening on port 3000");
+});

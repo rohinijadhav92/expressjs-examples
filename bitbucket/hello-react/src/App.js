0bea00c593eaf7ea9246f2b05f41acc84e330eea
Rohini <rohinijadhavdeopur@gmail.com>
2017-02-04 10:39:02 +0530

    Application on ReactJS

---
+++ bitbucket/hello-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ProjectBody from './componets/ProjectBody';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       my main project
+       <ProjectBody/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ bitbucket/hello-react/src/componets/ProjectBody.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class ProjectBody extends Component {
+  render() {
+    return (
+      <div className="ProjectBody">
+       my main ProjectBody
+      </div>
+    );
+  }
+}
+
+export default ProjectBody;
